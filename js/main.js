01baf8ff4e2d4b2cfae8610b746d215cafa90785
Ismael KDB <ismaelkondombo1@gmail.com>
2022-03-08 12:29:20 +0000

    form modifiction and remove rocket

--- js/main.js
+++ js/main.js
@@ -8,6 +8,8 @@ const placeholderS=document.querySelectorAll('.placeholderS');
 const info=document.querySelectorAll('.info');
 const forms=document.querySelector('#forms');
 const submit=document.querySelector('#submit');
+const wrapper=document.querySelector('.wrapper');
+const modal=document.querySelector('.modal');
 
 
 //handle input placeholder
@@ -25,33 +27,38 @@ inputWrapper.forEach((item,index)=>{
     submit.addEventListener('click',(e)=>{
         let _isvalid=forms.checkValidity()
         if(!_isvalid){
-            handleView(index);
+            item.addEventListener('input',()=>{
+                handleView(index);
+            })
         }
     })
 })
 window.addEventListener('click',(e)=>{
     outsideClick(e)
 })
-
 //handle select placeholder
 selectwrapper.forEach((item,index)=>{
     item.addEventListener('click',()=>{
         handleSelect(index);
     })
 })
+//handle thanks message
+submit.addEventListener('click',(e)=>{
+    handleSub(e)
+})
 
 function handleView(index){
-    input[index].addEventListener('input',(e)=>{
-        if(e.data!=''){
-            placeholder[index].style.opacity='0'
-        }
-    })
+    const _transform ="translate3d(0px,-38px, 0) ";
+    placeholder[index].style.transform = _transform;
+    placeholder[index].style.webkitTransform = _transform;
+    placeholder[index].style.msTransform = _transform;
+    placeholder[index].style.fontSize="15px";
 }
 function handleTab(index){
     if(index<5){
         input[index+1].addEventListener('input',(e)=>{
             if(e.data!=''){
-                placeholder[index+1].style.opacity='0'
+                handleView(index+1)
             }
         })
     }
@@ -59,7 +66,12 @@ function handleTab(index){
 function outsideClick(e){
     for (let i = 0; i< inputWrapper.length; i++) {
         if(e.target!=input[i] && input[i].value==''){
-            placeholder[i].style.opacity='1'
+            // placeholder[i].style.opacity='1'
+            const _transform ="translate3d(0px,0px, 0) ";
+            placeholder[i].style.transform = _transform;
+            placeholder[i].style.webkitTransform = _transform;
+            placeholder[i].style.msTransform = _transform;
+            placeholder[i].style.fontSize="25px";
         }
     }
 }
@@ -71,4 +83,29 @@ function handleSelect(index){
     else{
         placeholderS[index].style.opacity='1'
     }
+}
+function handleSub(e){
+    let _isvalid=forms.checkValidity()
+    if(_isvalid){
+        e.preventDefault()
+        document.body.style.overflowY='hidden'
+        wrapper.style.opacity="0";
+        setTimeout(() => {
+            modal.style.display='block'
+            setTimeout(() => {
+                modal.style.opacity="1";
+            }, 30);
+        }, 810);
+    }
+}
+function checkRefresh() {
+    if (document.location.hash === '#visited') {
+        return true;
+    } else {
+        document.location.hash = 'visited';
+        return false;
+    }
+}
+if(checkRefresh){
+    forms.reset();
 }
